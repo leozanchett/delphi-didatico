ae24f0ca817d31fb3845a6a1b7381b9e0dd1deee
leoB <leonardozandrade@gmail.com>
2021-06-24 21:39:01 -0300

    Toolbars & MultiView

--- Bootcamp 2018/week2/commom controls/frmTToolbareMultView.pas
+++ Bootcamp 2018/week2/commom controls/frmTToolbareMultView.pas
@@ -4,11 +4,19 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.MultiView;
 
 type
   TForm6 = class(TForm)
-    Image1: TImage;
+    ToolBar1: TToolBar;
+    TitleText: TText;
+    Button1: TButton;
+    Button2: TButton;
+    MultiView1: TMultiView;
+    Rectangle1: TRectangle;
+    Circle1: TCircle;
+    detailRectangle: TRectangle;
   private
     { Private declarations }
   public
