60a4e65604f5a4d9fb9e34eca21359c9147c35f3
leoB <leonardozandrade@gmail.com>
2021-11-28 20:18:09 -0300

    reference to function

--- Object Pascal Handbook 10.4.1 (Marco Cantu)/15 - Anonymous Methods/Unit1.pas
+++ Object Pascal Handbook 10.4.1 (Marco Cantu)/15 - Anonymous Methods/Unit1.pas
@@ -10,12 +10,15 @@ uses
 type
    TIntProc = reference to procedure(const N: Integer);
    TIntMethod = procedure(N: Integer) of object;
-   
+   TTrueOrFalse = reference to function(const _A1, _A2: Integer): Boolean;
+
    TForm1 = class(TForm)
       Memo1: TMemo;
       Button1: TButton;
       procedure Button1Click(Sender: TObject);
+      procedure FormCreate(Sender: TObject);
    private
+      FTrueOrFalse: TTrueOrFalse;
       { Private declarations }
       procedure CallTwice(_AValue: Integer; const _AProcedure: TIntProc);
    public
@@ -39,6 +42,7 @@ begin
       end;
    AnIntProc(22);
    CallTwice(1, AnIntProc);
+   FTrueOrFalse(13, 11);
 end;
 
 procedure TForm1.CallTwice(_AValue: Integer; const _AProcedure: TIntProc);
@@ -46,6 +50,15 @@ begin
    _AProcedure(_AValue);
    Inc(_AValue);
    _AProcedure(_AValue);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+   FTrueOrFalse :=  function(const _A1, _A2: Integer): Boolean
+   begin
+      if _A1 > _A2 then
+         ShowMessage(_A1.ToString + ' é maior do que ' + _A2.ToString);
+   end;
 end;
 
 end.
